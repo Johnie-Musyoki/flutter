30c01f0bbb10ad42b935ce53647fc915a9701828
kaiyuku <jhnmusyoki@gmail.com>
2023-01-31 09:12:42 +0300

    hello flutter

---
+++ hello.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main(){
+runApp( MyApp());
+}
+class MyApp extends StatelessWidget{
+	Widget Build(BuildContext Context){
+	  return MaterialApp(home:(text:'Hello John'),);
+	}
+}
